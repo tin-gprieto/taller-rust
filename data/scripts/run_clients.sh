9253591b9ca37ba2c83cf1afeba7bf2a0793a1cc
Alexis094 <72041462+Alexis094@users.noreply.github.com>
2024-08-13 01:47:04 -0300

    se mejora la distribucion

--- data/scripts/run_clients.sh
+++ data/scripts/run_clients.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-for DRONE in 1 2 3 4 5 6 7 8 9 10 11 12 13
-do
-    echo "Running DRONE: <$DRONE> ..."
-    gnome-terminal -- sh -c "cargo drone$DRONE;bash"
-	sleep 1
-done
+echo "Running MONITORING_APP ..."
+gnome-terminal -- sh -c "cargo monitoring_app;bash"
+sleep 3
+
+echo "Running CAMS_SYSTEM ..."
+gnome-terminal -- sh -c "cargo cams_system;bash"
+sleep 2
 
 
--- data/scripts/run_server.sh
+++ data/scripts/run_server.sh
@@ -4,12 +4,11 @@ echo "Running BROKER ... "
 gnome-terminal -- sh -c "cargo broker;bash"
 sleep 2
 
-echo "Running MONITORING_APP ..."
-gnome-terminal -- sh -c "cargo monitoring_app;bash"
-sleep 3
+read  -n 1 -p "PRESIONA UNA TECLA CUANDO MONITORING APP SE HAYA CONECTADO ... "
 
-echo "Running CAMS_SYSTEM ..."
-gnome-terminal -- sh -c "cargo cams_system;bash"
-sleep 2
-
-#read  -n 1 -p "PRESIONA UNA TECLA CUANDO MONITORING APP SE HAYA CONECTADO ... "
+for DRONE in 1 2 3 4 5 6 7 8 9 10 11 12 13
+do
+    echo "Running DRONE: <$DRONE> ..."
+    gnome-terminal -- sh -c "cargo drone$DRONE;bash"
+	sleep 1
+done
