72e10676bdbda6d1e3d026d9b6dd82287b1e424f
Alexis094 <ramos.ale094@gmail.com>
2024-08-13 01:55:26 -0300

    script fix

--- data/scripts/run_server.sh
+++ data/scripts/run_server.sh
@@ -5,6 +5,7 @@ gnome-terminal -- sh -c "cargo broker;bash"
 sleep 2
 
 read  -n 1 -p "PRESIONA UNA TECLA CUANDO MONITORING APP SE HAYA CONECTADO ... "
+echo " "
 
 for DRONE in 1 2 3 4 5 6 7 8 9 10 11 12 13
 do
