d2aab00c4b630c64a841dcdc063fce44cc2e7489
Alexis094 <72041462+Alexis094@users.noreply.github.com>
2024-08-13 01:22:35 -0300

    bug fix

--- data/scripts/run_clients.sh
+++ data/scripts/run_clients.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-echo "Runiing CAM SYSTEM and MONITORING_APP ..."
-gnome-terminal -- sh -c "cargo cams_system;bash"
-sleep 3
+for DRONE in 1 2 3 4 5 6 7 8 9 10 11 12 13
+do
+    echo "Running DRONE: <$DRONE> ..."
+    gnome-terminal -- sh -c "cargo drone$DRONE;bash"
+	sleep 1
+done
 
-gnome-terminal -- sh -c "cargo monitoring_app;bash"
+
